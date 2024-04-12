7cf246b4e4c01a87d4ad3c5c00519adb3f604d70
Erin Yant <ghosrider383@gmail.com>
2024-04-11 18:29:38 -0600

    completed

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((data) => renderBooks(data))
 }
 
 function renderBooks(books) {
@@ -15,3 +18,32 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+
+
+
+// // Find the 5th book in series
+// fetch("https://anapioficeandfire.com/api/books")
+//   .then((response) => response.json())
+//   .then((json) => {
+//     const books = json
+//     const fifthBook = books[4] 
+//       console.log(fifthBook)
+//   })
+
+//   // Find 1031st character in the series
+
+// // Find total number of pages in all books
+//   fetch("https://anapioficeandfire.com/api/books")
+//   .then((response) => response.json())
+//   .then((json) => {
+//     const books = json
+//     let totalPages = 0
+
+//     books.forEach((book) => {
+//       totalPages += book.numberOfPages
+//     }) 
+//       console.log("Total number of pages:", totalPages)
+//   })
+
